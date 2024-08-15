88467d31a1d61745b3c266b0a0e4216434b7345a
syed-jaid <syed@frqpharmacy.com>
2024-08-16 00:08:52 +0600

    code adedd

--- src/components/app/sections/Features.js
+++ src/components/app/sections/Features.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React from "react";
+import Shipstation from "../../medias/shipstation.png";
+import Veeqo from "../../medias/veeqo.png";
 
 const Features = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello Features!
-            </h1>
+  return (
+    <div>
+      <p className="text-3xl text-center font-bold ">
+        <span className="">Features</span> you Will Love
+      </p>
+
+      <div className="flex bg-[#8fa3bb] p-2">
+        <div className="bg-[#FFFFFF] p-3 rounded-md">
+          <img src={Shipstation}></img>
         </div>
-    );
+        <div>
+          <img src={Veeqo}></img>
+        </div>
+      </div>
+
+      <div>
+        <img></img>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
